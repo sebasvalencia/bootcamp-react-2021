0b0c1a9acfdc14fda3070df6a54d0b180d6aff95
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2021-10-22 21:45:15 -0500

    feat: modify pokemon list component

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,27 +1,32 @@
 import { useContext } from "react";
+import PokemonContextActions from "../../context/pokemonContext/actions";
 import { PokemonContext } from "../../context/pokemonContext/context";
 import "./pokemonList.scss";
 
 const PokemonList = () => {
 
-    const {state: {pokemons}} = useContext(PokemonContext);
+    const {dispatch, state: {filterPokemons}} = useContext(PokemonContext);
 
-    console.log('pokemons', pokemons);
-
-    const openInformationPokemon = (url) => {
-        console.log('click', url);
+    const openInformationPokemon = (pokemon) => {
+        dispatch({
+            type: PokemonContextActions.setSelectedPokemon,
+            results: pokemon
+        })
     }
 
     return (
         <div className="pokemon-list">
             <ul className="ul-list">
                 {
-                    pokemons.map( ({name, url}) => <li key={name} onClick={() => openInformationPokemon(url)}>{name}</li> )
+                    filterPokemons.length >=1 ?
+                    filterPokemons.map( (pokemon, i) => 
+                    <li key={i} onClick={() => openInformationPokemon(pokemon)}>{pokemon.name}</li> 
+                    ) :
+                    <div>no results found</div>
                 }
             </ul>
         </div>
     )
-
 }
 
 export default PokemonList;
